d575a4151b0f85deae770c206130b83160a22121
Trevor Suarez <rican7@gmail.com>
2021-10-20 00:06:49 -0600

    Adding content for the about me page

--- src/pages/about-us/index.svelte
+++ src/pages/about-us/index.svelte
@@ -4,15 +4,38 @@
 
 <Hero />
 
-<div class="container">
-  <h1>About Us</h1>
+<div class="page-width-wrapper">
+  <div class="container">
+    <h1>About Us</h1>
 
-  <p>Content here...</p>
+    <p>
+      Founded in Denver, Colorado, in 2021, three friends came up with the idea
+      to create something fresh and new just for you.
+    </p>
+    <p />
+    <p>
+      Inspired by the natural world, modern events, and literally whatever one
+      asshole said to the other over a glass of bourbon, we decided that this
+      brand needed to come to life.
+    </p>
+    <p>
+      So, here we are, with our first season of apparel, and many more to come.
+      We hope you vibe with us, and stay tuned.
+    </p>
+  </div>
 </div>
 
 <style>
   .container {
     margin: 20px 0px;
     text-align: center;
+  }
+  p {
+    max-width: 500px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0 5%;
+    font-size: var(--important-font-size);
+    text-align: justify;
   }
 </style>
