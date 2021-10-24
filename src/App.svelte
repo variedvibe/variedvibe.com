800b341d84af5834968e9b8fd7b7bd32e81172c8
Trevor Suarez <rican7@gmail.com>
2021-10-23 18:18:23 -0600

    Adding Google Analytics tracking

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,15 @@
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
 
+  import { GoogleAnalytics } from "@beyonk/svelte-google-analytics";
+
   import Serviceworker from "./components/Serviceworker.svelte";
 
   import _ from "./global.css"; // Global styles
+
+  const googleAnalyticsId = "G-9TWM0P60LB";
 </script>
 
 <Router {routes} />
+<GoogleAnalytics properties={[googleAnalyticsId]} />
 <Serviceworker />
