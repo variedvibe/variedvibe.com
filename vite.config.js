d756908b2d7ed0d1a921bfead0b4fdd8c7babada
Trevor Suarez <rican7@gmail.com>
2021-10-23 20:21:22 -0600

    Injecting the baseURL into the built HTML

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-const { appConfig } = require('./package.json')
-const viteMainJs = require('vite-main-js')
-const { svelte } = require('@sveltejs/vite-plugin-svelte')
-const { port } = appConfig
-const production = process.env.NODE_ENV === 'production'
+const { appConfig } = require("./package.json");
+const viteMainJs = require("vite-main-js");
+const { loadEnv } = require("vite");
+const { svelte } = require("@sveltejs/vite-plugin-svelte");
+const { injectHtml } = require("vite-plugin-html");
+
+const { port } = appConfig;
+
+process.env = {
+  ...process.env,
+  ...loadEnv(process.env.NODE_ENV, process.cwd()),
+};
+
+const production = process.env.NODE_ENV === "production";
+const baseUrl = process.env.VITE_BASE_URL ?? process.env.BASE_URL;
+
 module.exports = {
   server: {
     port: port,
@@ -11,10 +22,10 @@ module.exports = {
     cssCodeSplit: false,
   },
   optimizeDeps: {
-    exclude: ['@roxi/routify'],
+    exclude: ["@roxi/routify"],
   },
   resolve: {
-    dedupe: ['@roxi/routify'],
+    dedupe: ["@roxi/routify"],
   },
   plugins: [
     viteMainJs(),
@@ -23,5 +34,10 @@ module.exports = {
       emitCss: true,
       hot: !production,
     }),
+    injectHtml({
+      data: {
+        baseUrl: baseUrl,
+      },
+    }),
   ],
-}
+};
