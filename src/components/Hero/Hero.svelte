4605c04254ce20d65b2a8ae9b7c78f0441fc4385
Trevor Suarez <rican7@gmail.com>
2021-10-26 20:12:18 -0600

    Tweaking the hero VIBE overlay shadow

--- src/components/Hero/Hero.svelte
+++ src/components/Hero/Hero.svelte
@@ -74,7 +74,10 @@
     background-position: center;
     background-size: contain;
     backdrop-filter: contrast(0.85);
-    filter: drop-shadow(0px 0px 5px var(--gray-mid-darker));
+    filter: drop-shadow(1px 0px 0px var(--gray-mid-darker))
+      drop-shadow(0px 1px 0px var(--gray-mid-darker))
+      drop-shadow(-1px 0px 0px var(--gray-mid-darker))
+      drop-shadow(0px -1px 0px var(--gray-mid-darker));
     z-index: 20;
   }
 </style>
