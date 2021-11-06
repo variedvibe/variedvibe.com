3c14274c93c11f8fcde477a45b4977c95ad404a0
Trevor Suarez <rican7@gmail.com>
2021-11-06 00:33:31 -0600

    Adding a "Special Thanks" section to our About Us

--- src/pages/about-us/index.svelte
+++ src/pages/about-us/index.svelte
@@ -4,36 +4,77 @@
 
 <Hero />
 
-<div id="container" class="page-width-wrapper">
-  <h2>About Us</h2>
+<div class="container page-width-wrapper">
+  <section class="main">
+    <h2>About Us</h2>
 
-  <p>
-    Founded in Denver, Colorado, in 2021, three friends came up with the idea to
-    create something fresh and new just for you.
-  </p>
-  <p />
-  <p>
-    Inspired by the natural world, modern events, and literally whatever one
-    asshole said to the other over a glass of bourbon, we decided that this
-    brand needed to come to life.
-  </p>
-  <p>
-    So, here we are, with our first season of apparel, and many more to come. We
-    hope you vibe with us, and stay tuned.
-  </p>
+    <p>
+      Founded in Denver, Colorado, in 2021, three friends came up with the idea
+      to create something fresh and new just for you.
+    </p>
+    <p />
+    <p>
+      Inspired by the natural world, modern events, and literally whatever one
+      asshole said to the other over a glass of bourbon, we decided that this
+      brand needed to come to life.
+    </p>
+    <p>
+      So, here we are, with our first season of apparel, and many more to come.
+      We hope you vibe with us, and stay tuned.
+    </p>
+  </section>
+  <hr />
+  <section>
+    <h3>Special Thanks</h3>
+
+    <p>
+      Varied Vibe. wouldn't exist today without the help of our friends and
+      parners. With that said, we'd like to give a special thanks to the
+      following people.
+    </p>
+
+    <ul class="thanks-list">
+      <li>
+        <a
+          title="Instagram - Aurora Photo"
+          target="_blank"
+          rel="noopener"
+          href="https://www.instagram.com/auroraprophoto/"
+          >Aurora Adams; <em>Model Photography</em></a
+        >
+      </li>
+      <li>Our various models; friends and partners in style</li>
+      <li>Denver; the inspiration</li>
+    </ul>
+  </section>
 </div>
 
 <style>
-  #container {
-    margin: 20px auto;
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     text-align: center;
   }
-  p {
+  section,
+  hr {
     max-width: 500px;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 0 5%;
-    font-size: var(--important-font-size);
+    margin: 0 5%;
+  }
+  hr {
+    width: 90%;
+    margin: 25px 5%;
+  }
+  section p {
     text-align: justify;
+  }
+  .main p {
+    font-size: var(--important-font-size);
+  }
+  .thanks-list {
+    text-align: left;
+  }
+  .thanks-list em {
+    color: var(--gray-mid);
   }
 </style>
