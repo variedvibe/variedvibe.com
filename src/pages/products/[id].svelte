c6a00664a3fc8f0d65bbb9811916ab4c964e327d
Trevor Suarez <rican7@gmail.com>
2021-12-26 17:48:17 -0700

    Adding support for product ID URLs

---
+++ src/pages/products/[id].svelte
@@ -0,0 +1,80 @@
+<script>
+  import { goto, params, redirect } from "@roxi/routify";
+
+  import { getProductById, getProductBySlug } from "/src/services/Shop/Shop.js";
+  import { shopifyProductGid } from "/src/services/Shop/gid.js";
+  import { ErrorTypes, getErrorType } from "/src/services/Shop/errors.js";
+
+  import { pageMeta } from "/src/components/Meta/PageMeta/stores.js";
+  import ProductDetail from "/src/components/Product/ProductDetail.svelte";
+  import LoadingMessage from "/src/components/StatusMessage/LoadingMessage.svelte";
+  import StatusMessage, {
+    messageErrorGeneric,
+  } from "/src/components/StatusMessage/StatusMessage.svelte";
+
+  export let id;
+
+  let productFetcher;
+  let productFetcherParam;
+
+  // Use numeric IDs, but support "slug" IDs for backwards compatibility.
+  //
+  // (We used to link to product slugs directly, but those are more likely to
+  // change than the numeric IDs, so its more apt to break links. Still, we
+  // don't want to break old links, so we support both.)
+  if (id.match(/^[0-9]+$/)) {
+    productFetcher = getProductById;
+    productFetcherParam = shopifyProductGid(id);
+  } else {
+    productFetcher = getProductBySlug;
+    productFetcherParam = id;
+  }
+
+  async function fetchProduct() {
+    let product;
+
+    try {
+      product = await productFetcher(productFetcherParam);
+    } catch (e) {
+      if (getErrorType(e) === ErrorTypes.UnknownProduct) {
+        $goto("/404", null, true, false);
+        return;
+      }
+
+      throw e;
+    }
+
+    // If the URL id or slug doesn't match the product, redirect so that we are
+    // always displaying the canonical product URL.
+    if (id !== product.gid.id || $params.slug !== product.slug) {
+      $redirect(`./${product.gid.id}/${product.slug}`);
+      return;
+    }
+
+    $pageMeta.title = product.name;
+    $pageMeta.description = product.description;
+    $pageMeta.image = product.images[0]?.src;
+
+    return product;
+  }
+
+  export let product = fetchProduct();
+</script>
+
+<div id="container" class="page-width-wrapper">
+  {#await product}
+    <LoadingMessage />
+  {:then Product}
+    {#if Product}
+      <ProductDetail product={Product} />
+    {/if}
+  {:catch}
+    <StatusMessage message={messageErrorGeneric} />
+  {/await}
+</div>
+
+<style>
+  #container {
+    margin: 0 auto;
+  }
+</style>
---
+++ src/pages/products/[id]/[slug].svelte
@@ -0,0 +1,16 @@
+<script>
+  import { params, goto } from "@roxi/routify";
+
+  const id = $params.id;
+
+  // We use this page as a sort of "alias" URL. Basically a "pretty" URL similar
+  // to Amazon product URLs and StackOverflow question URLs.
+  //
+  // We show this "slug" at the end for human-reading, but don't depend on it as
+  // they're more prone to change than product IDs, so we actually show the
+  // product ID page here.
+  //
+  // Use `goto` to show the product ID page, passing in the params so that the
+  // product ID page has access to the slug, without an actual redirect.
+  $goto(`../${id}`, $params, true, true);
+</script>
