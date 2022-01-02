9fccc09febf5ae872b88eae791b3d4184868a66a
Trevor Suarez <rican7@gmail.com>
2022-01-02 03:03:02 -0700

    Adding a new page navigation indicator component
    We'll now show a loading bar when navigating between different pages.

---
+++ src/components/PageNavIndicator.svelte
@@ -0,0 +1,84 @@
+<script>
+  import { isChangingPage, beforeUrlChange } from "@roxi/routify";
+
+  // The time interval, in milliseconds, to use to simulate load updates.
+  const loadSimulationIntervalTime = 200;
+
+  // TODO: Remove in favor of just using `$isChangingPage` once issue
+  // https://github.com/roxiness/routify/issues/421 is fixed.
+  $: isLoading = $isChangingPage;
+  $beforeUrlChange(() => (isLoading = true));
+
+  let percentageLoaded = 0;
+
+  let loadSimulationInterval;
+
+  $: {
+    if (isLoading && !loadSimulationInterval) {
+      percentageLoaded = 0;
+
+      loadSimulationInterval = setInterval(() => {
+        // Update the percent loaded, but stop at 99% so we don't show a 100%
+        // when we're not actually fully loaded.
+        if (percentageLoaded < 99) {
+          percentageLoaded++;
+        }
+      }, loadSimulationIntervalTime);
+    } else if (!isLoading) {
+      if (loadSimulationInterval) {
+        // Stop simulating loading
+        clearInterval(loadSimulationInterval);
+        loadSimulationInterval = null;
+      }
+
+      percentageLoaded = 100;
+    }
+  }
+</script>
+
+<progress
+  id="page-nav-indicator"
+  class:hidden={!isLoading}
+  max="100"
+  value={percentageLoaded}
+  style="--load-simulation-interval-time: {loadSimulationIntervalTime}ms"
+/>
+
+<style>
+  progress {
+    --progress-value-transition: width var(--load-simulation-interval-time) ease;
+
+    -webkit-appearance: none;
+    appearance: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10000;
+    display: block;
+    width: 100%;
+    height: 2px;
+    margin: 0;
+    padding: 0;
+    border: none;
+    color: dodgerblue; /* TODO: Change to site-wide accent color once available */
+    accent-color: currentColor;
+    transition: opacity var(--animation-speed-normal)
+      var(--animation-timing-function-natural);
+  }
+  progress,
+  progress::-webkit-progress-bar {
+    background-color: transparent;
+  }
+  progress::-webkit-progress-value {
+    background-color: currentColor;
+    transition: var(--progress-value-transition);
+  }
+  progress::-moz-progress-bar {
+    background-color: currentColor;
+    transition: var(--progress-value-transition);
+  }
+  .hidden {
+    opacity: 0;
+  }
+</style>
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,12 +1,15 @@
 <script>
   import BrowserHints from "/src/components/Meta/Performance/BrowserHints.svelte";
   import PageMeta from "/src/components/Meta/PageMeta/PageMeta.svelte";
+  import PageNavIndicator from "/src/components/PageNavIndicator.svelte";
   import Header from "/src/components/Header/Header.svelte";
   import Footer from "/src/components/Footer/Footer.svelte";
 </script>
 
 <BrowserHints />
 <PageMeta />
+
+<PageNavIndicator />
 
 <Header />
 
