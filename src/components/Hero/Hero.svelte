77ee78ea302952839a4b3e65b8127ceeee39fd28
Trevor Suarez <rican7@gmail.com>
2022-07-18 23:00:33 -0600

    Updating the hero images and component

--- src/components/Hero/Hero.svelte
+++ src/components/Hero/Hero.svelte
@@ -1,15 +1,15 @@
 <script>
-  const defaultBackgroundColor = "#e0e0e0"; // Hard-coded to match the image
-  const defaultHeight = 320;
+  const defaultBackgroundColor = "#e8e9ed"; // Hard-coded to match the image
+  const defaultHeight = 420;
   const defaultImageSrc =
-    "/assets/hero-images/reeves-flower-uniform-background.jpg";
+    "/assets/hero-images/pops-tee-uniform-background-crop.jpg";
   const defaultImageSrcSet = [
-    "/assets/hero-images/reeves-flower-uniform-background_250x.jpg 250w",
-    "/assets/hero-images/reeves-flower-uniform-background_500x.jpg 500w",
-    "/assets/hero-images/reeves-flower-uniform-background_750x.jpg 750w",
-    "/assets/hero-images/reeves-flower-uniform-background_1000x.jpg 1000w",
-    "/assets/hero-images/reeves-flower-uniform-background_2000x.jpg 2000w",
-    "/assets/hero-images/reeves-flower-uniform-background_4000x.jpg 4000w",
+    "/assets/hero-images/pops-tee-uniform-background-crop_250x.jpg 250w",
+    "/assets/hero-images/pops-tee-uniform-background-crop_500x.jpg 500w",
+    "/assets/hero-images/pops-tee-uniform-background-crop_750x.jpg 750w",
+    "/assets/hero-images/pops-tee-uniform-background-crop_1000x.jpg 1000w",
+    /*"/assets/hero-images/pops-tee-uniform-background-crop_2000x.jpg 2000w",*/
+    /*"/assets/hero-images/pops-tee-uniform-background-crop_4000x.jpg 4000w",*/
   ].join(", ");
 
   export let backgroundColor = defaultBackgroundColor;
@@ -17,7 +17,8 @@
   export let imageSrc = defaultImageSrc;
   export let imageSrcSet = "";
   export let addNoise = true;
-  export let addVibeOverlay = true;
+  export let addVibeOverlay = false;
+  export let addLogoOverlay = true;
 
   if (!imageSrcSet && imageSrc === defaultImageSrc) {
     imageSrcSet = defaultImageSrcSet;
@@ -26,13 +27,13 @@
 
 <section
   id="hero"
-  style="--background-color: {backgroundColor}; --height: {height}px;"
+  style="--background-color: {backgroundColor}; --height: min({height}px, var(--max-hero-height));"
 >
   <img
     alt="hero"
     src={imageSrc}
     srcset={imageSrcSet}
-    sizes="max(100vw, {height}px)"
+    sizes="max(100vw, min({height}px, var(--max-hero-height)))"
     {height}
   />
   {#if addNoise}
@@ -40,6 +41,9 @@
   {/if}
   {#if addVibeOverlay}
     <div id="vibe-overlay" class="overlay" />
+  {/if}
+  {#if addLogoOverlay}
+    <div id="logo-overlay" class="overlay" />
   {/if}
 </section>
 
@@ -51,6 +55,7 @@
     position: relative;
     width: 100%;
     height: var(--height);
+    max-height: var(--max-hero-height);
     align-content: center;
     align-items: center;
     justify-content: center;
@@ -84,6 +89,19 @@
     background-repeat: no-repeat;
     background-position: center;
     background-size: contain;
+    backdrop-filter: contrast(0.85);
+    -webkit-backdrop-filter: contrast(0.85);
+    filter: drop-shadow(1px 0px 0px var(--gray-mid-darker))
+      drop-shadow(0px 1px 0px var(--gray-mid-darker))
+      drop-shadow(-1px 0px 0px var(--gray-mid-darker))
+      drop-shadow(0px -1px 0px var(--gray-mid-darker));
+    z-index: 20;
+  }
+  #logo-overlay {
+    background-image: url("/assets/mark-black.svg");
+    background-repeat: no-repeat;
+    background-position: 20px 10px;
+    background-size: 100px;
     backdrop-filter: contrast(0.85);
     -webkit-backdrop-filter: contrast(0.85);
     filter: drop-shadow(1px 0px 0px var(--gray-mid-darker))
